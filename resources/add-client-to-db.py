601db6b15aef607309c7ab21a0a7060db586d419
neil <neil@elneelo.com>
2016-12-03 12:30:23 +0000

    some changes required for ease of use with flask

--- resources/add-client-to-db.py
+++ resources/add-client-to-db.py
@@ -3,30 +3,30 @@ import datetime
 from flask.ext.pymongo import Connection
 
 connection = Connection()
-db = connection['client-database']
-collection = db['client-collection']
+db = connection['dist']
+collection = db['client']
 
-clients = [{"id": "1"
+clients = [{"client_id": "1"
     ,"session_key": "928F767EADE2DBFD62BFCD65B8E21"
     ,"session_key_expires": (datetime.utcnow() + timedelta(seconds=60*5)).strftime('%Y-%m-%d %H:%M:%S')
     ,"public_key": "6EC46E3DC79A2D4DBE79151C2DCE1"
     ,"password": "0dP1jO2zS7"},
-    {"id": "2"
+    {"client_id": "2"
     ,"session_key": "98D4BD8C1EAFAEA54A1D818D95BA1"
     ,"session_key_expires": (datetime.utcnow() + timedelta(seconds=60*5)).strftime('%Y-%m-%d %H:%M:%S')
     ,"public_key": "D9EEE7B719786189C6F449BBE9714"
     ,"password": "GhN9bZnouL"},
-    {"id": "3"
+    {"client_id": "3"
     ,"session_key": "8A3A915849C95AC11F67DACA8EB89"
     ,"session_key_expires": (datetime.utcnow() + timedelta(seconds=60*5)).strftime('%Y-%m-%d %H:%M:%S')
     ,"public_key": "6EAF254F31C1D1B91766CFBFAC928"
     ,"password": "f9C1731KLs"},
-    {"id": "4"
+    {"client_id": "4"
     ,"session_key": "CC6C95A928319BFC816BEDF2A2BBC"
     ,"session_key_expires": (datetime.utcnow() + timedelta(seconds=60*5)).strftime('%Y-%m-%d %H:%M:%S')
     ,"public_key": "DA73157ACAB711738E179BE5F8392"
     ,"password": "P6ugR77H5P"},
-    {"id": "5"
+    {"client_id": "5"
     ,"session_key": "751C792CB49DF4C859E6F4F3A1EFA"
     ,"session_key_expires": (datetime.utcnow() + timedelta(seconds=60*5)).strftime('%Y-%m-%d %H:%M:%S')
     ,"public_key": "6E9B98EE737127C561725E16B36A2"
