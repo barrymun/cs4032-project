2a52bc19fa007adea42723b473f31d6a460a315c
neil <neil@elneelo.com>
2016-12-03 21:06:57 +0000

    final fixes

--- dir-server/dir-server.py
+++ dir-server/dir-server.py
@@ -2,11 +2,13 @@ import base64
 import md5
 import datetime
 import json
-
+import hashlib
+import flask
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask_pymongo import PyMongo
+from flask import Response
+from flask.ext.pymongo import PyMongo
 
 application = Flask(__name__)
 mongo = PyMongo(application)
@@ -14,23 +16,30 @@ mongo = PyMongo(application)
 # constants
 AUTH_SERVER_STORAGE_SERVER_KEY = "d41d8cd98f00b204e9800998ecf8427e"
 
+def reset():
+    db = mongo.db.server
+    db.directories.drop()
+    db.files.drop()
 
 @application.route('/server/file/upload', methods=['POST'])
 def file_upload():
+    reset()
     data = request.get_data()
     headers = request.headers
     filename = headers['filename']
     directory_name = headers['directory']
 
-    db = mongo.db.server
-    directory = None
-    if not db.directories.find({"name": directory_name,"reference": md5(directory_name).hexdigest()}):
+    m = hashlib.md5()
+    m.update(directory_name)
+    if not mongo.db.server.directories.find_one({"name": directory_name, "reference": m.hexdigest()}):
         directory = Directory.create(directory_name)
+    else:
+        directory = mongo.db.server.directories.find_one({"name": directory_name, "reference": m.hexdigest()})
 
-    file = None
-    if not db.files.find({"name": filename, "directory": directory['reference']}):
+    if not mongo.db.server.files.find_one({"name": filename, "directory": directory['reference']}):
         file = File.create(filename, directory['name'], directory['reference'])
-
+    else:
+        file = mongo.db.server.files.find_one({"name": filename, "directory": directory['reference']})
     with open(file["reference"], "wb") as fo:
         fo.write(data)
 
@@ -39,7 +48,12 @@ def file_upload():
 
 @application.route('/server/file/download', methods=['POST'])
 def file_download():
-    pass
+    reset()
+    data = request.get_json(force=True)
+    ticket = data.get('auth_token')
+    return flask.send_file("672058a02be07e598490957388e83bec")
+
+
 
 class File:
     def __init__(self):
@@ -48,11 +62,13 @@ class File:
     @staticmethod
     def create(name, directory_name, directory_reference):
         db = mongo.db.server
-        file = db.files.insert({"name":name,
-            "directory": directory_reference,
-            "reference": md5(directory_reference + "/" + directory_name).hexdigest(),
-            "updated_at": datetime.datetime.utcnow()
-        })
+        m = hashlib.md5()
+        m.update(directory_reference + "/" + directory_name)
+        db.files.insert({"name":name,
+                                "directory":directory_reference,
+                                "reference": m.hexdigest(),
+                                "updated_at": datetime.datetime.utcnow()})
+        file = db.files.find_one({"reference":m.hexdigest()})
         return file
 
 class Directory:
@@ -62,9 +78,13 @@ class Directory:
     @staticmethod
     def create(name):
         db = mongo.db.server
-        db.directories.insert({"name":name
-            ,"reference": md5(name).hexdigest()
-        })
+        m = hashlib.md5()
+        m.update(name)
+        db.directories.insert({"name":name, "reference": m.hexdigest()})
+        directory = db.directories.find_one({"name":name, "reference": m.hexdigest()})
+        return directory
+
+
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1',port=8093)
