50481d6338be54f7bff4c5d856621fdddeae7c7c
neil <neil@elneelo.com>
2016-12-03 12:01:11 +0000

    init commit

---
+++ auth-server.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+# noinspection PyUnresolvedReferences
+from flask.ext.pymongo import PyMongo
+from flask.ext.pymongo import Connection
+
+application = Flask(__name__)
+
+mongo = PyMongo(application)
+
+connection = Connection()
+db = connection['client-database']
+collection = db['client-collection']
+
+CLIENT_SERVER_KEY = "d41d8cd98f00b204e9800998ecf8427e"
+
+@application.route('/user/auth', methods=['POST'])
+def user_auth():
+    data = request.values
+    client_id = data.get('client_id')
+    encrypted_password = data.get('encrypted_password')
+
+
+    return jsonify({})
+
+
+class Authentication:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    application.run()
