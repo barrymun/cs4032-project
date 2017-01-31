2bbb54636a7cb7ff109d751af997e396869948ab
neil <neil@elneelo.com>
2017-01-31 18:18:21 +0000

    Third server added, as per description

--- authenticationserver.py
+++ authenticationserver.py
@@ -21,12 +21,18 @@ db.directories.drop()
 db.files.drop()
 db.transactions.drop()
 hash_key = hashlib.md5()
+
 hash_key.update("localhost" + ":" + "9001")
 db.servers.insert(
     {"reference": hash_key.hexdigest(), "host": "localhost", "port": "9001", "master_server": True, "in_use": False})
+
 hash_key.update("localhost" + ":" + "9002")
 db.servers.insert(
     {"reference": hash_key.hexdigest(), "host": "localhost", "port": "9002", "master_server": False, "in_use": False})
+
+hash_key.update("localhost" + ":" + "9002")
+db.servers.insert(
+    {"reference": hash_key.hexdigest(), "host": "localhost", "port": "9003", "master_server": False, "in_use": False})
 
 application = Flask(__name__)
 mongo_server = "localhost"
