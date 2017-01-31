6d604d34b5ce13e966a945699916f8ecb7df2b23
neil <neil@elneelo.com>
2017-01-31 12:41:20 +0000

    setup now included in the authentication server setup - auth server file renamed

--- authenticationserver.py
+++ authenticationserver.py
@@ -1,4 +1,5 @@
 import base64
+import hashlib
 import json
 import random
 import string
@@ -8,6 +9,24 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from pymongo import MongoClient
+
+mongo_server = "localhost"
+mongo_port = "27017"
+connect_string = "mongodb://" + mongo_server + ":" + mongo_port
+connection = MongoClient(connect_string)
+db = connection.project
+db.users.drop()
+db.servers.drop()
+db.directories.drop()
+db.files.drop()
+db.transactions.drop()
+hash_key = hashlib.md5()
+hash_key.update("localhost" + ":" + "9001")
+db.servers.insert(
+    {"reference": hash_key.hexdigest(), "host": "localhost", "port": "9001", "master_server": True, "in_use": False})
+hash_key.update("localhost" + ":" + "9002")
+db.servers.insert(
+    {"reference": hash_key.hexdigest(), "host": "localhost", "port": "9002", "master_server": False, "in_use": False})
 
 application = Flask(__name__)
 mongo_server = "localhost"
